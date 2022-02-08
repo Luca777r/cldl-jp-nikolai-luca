fabc9199a7383b28d29e578f7d4630f00f608d1c
Nikolai Pankov <pankoff2000@gmail.com>
2022-02-08 16:33:53 +0100

    ticket 2. front-controller

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'includes/header.php';
+
+$page = 'home';
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+
+include_once 'pages/' . $page . '.php';
+
+include_once 'includes/footer.php';
