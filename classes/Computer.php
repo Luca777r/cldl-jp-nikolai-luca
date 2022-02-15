4b64b28140a93c697e1b159f6752afbd41326c4a
Lucius <lucadelrosso.pro@gmail.com>
2022-02-15 11:48:35 +0100

    class computer 10

--- classes/Computer.php
+++ classes/Computer.php
@@ -1,9 +1,20 @@
 <?php
 
+use Traits\ComponentTrait;
 
-class Computer extends Component
+class Computer
 {
+    use ComponentTrait;
     protected ?bool $isLaptop;
+    protected ?string $nom;
+
+
+
+    function __construct(bool $isLaptop, string $nom)
+    {
+        $this->isLaptop = $isLaptop;
+        $this->nom = $nom;
+    }
 
     public function getIsLaptop(): bool
     {
@@ -13,6 +24,18 @@ class Computer extends Component
     public function setIsLaptop($isLaptop): self
     {
         $this->isLaptop = $isLaptop;
+
+        return $this;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom($nom): self
+    {
+        $this->nom = $nom;
 
         return $this;
     }
