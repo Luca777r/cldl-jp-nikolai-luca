d5855cb86e201d76bedb527aa81607e496a1b5e9
npankov <pankoff2000@gmail.com>
2022-02-22 16:04:37 +0100

    update alert connexion

--- includes/header.php
+++ includes/header.php
@@ -49,16 +49,6 @@ include 'variables/bd.php';
                     </li>
                 </ul>
             </div>
-            <?php
-            if (isset($_GET["loginSuccess"]) && $_GET["loginSuccess"] == 1) {
-            ?>
-                <div class="alert alert-success" role="alert">
-                    Vous êtes bien connecté
-                </div>
-
-            <?php
-            }
-            ?>
         </div>
     </nav>
 
