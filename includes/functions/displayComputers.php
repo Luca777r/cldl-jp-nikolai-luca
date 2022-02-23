af9c0bc4a1198b8b951329b1e30f7c8c297ed4ac
npankov <pankoff2000@gmail.com>
2022-02-23 10:12:48 +0100

    update psr-2

--- includes/functions/displayComputers.php
+++ includes/functions/displayComputers.php
@@ -1,38 +1,43 @@
 <?php
 
 function displayComputers()
-    {
-        ?><table class="table table-striped table-hover">
-  <thead class="table-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">name</th>
-      <th scope="col">quantity</th>
-      <th scope="col">islaptop</th>
-      <th scope="col">date</th>
-      <th scope="col">Supprimer</th>
-      <th scope="col">Editer</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach (getAllComputers() as $computer) {?>
+{
+    ?>
+    <table class="table table-striped table-hover">
+        <thead class="table-dark">
         <tr>
-      <th scope="row"><?= $computer->getId()?></th>
-      <td><?= $computer->getName()?></td>
-      <td><?= $computer->getQuantity()?></td>
-      <td><?= $computer->getIsLaptop()?></td>
-      <td><?= $computer->getDateAdd()->format("Y-m-d")?></td>
-      <td><a href="?page=designer&del=<?=$computer->getId()?>"><button class="btn btn-danger">Supprimer</button></a></td>
-      <td><a href="?page=update&id=<?=$computer->getId()?>"><button class="btn btn-success">Editer</button></a></td>
-    </tr>
-      <?php } ?>
-  </tbody>
-</table>
+            <th scope="col">#</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Quantité</th>
+            <th scope="col">Si portable</th>
+            <th scope="col">Date</th>
+            <th scope="col">Supprimer</th>
+            <th scope="col">Editer</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach (getAllComputers() as $computer) { ?>
+            <tr>
+                <th scope="row"><?= $computer->getId() ?></th>
+                <td><?= $computer->getName() ?></td>
+                <td><?= $computer->getQuantity() ?></td>
+                <td><?= $computer->getIsLaptop() ?></td>
+                <td><?= $computer->getDateAdd()->format("Y-m-d") ?></td>
+                <td><a href="?page=designer&del=<?= $computer->getId() ?>">
+                        <button class="btn btn-danger">Supprimer</button>
+                    </a></td>
+                <td><a href="?page=update&id=<?= $computer->getId() ?>">
+                        <button class="btn btn-success">Editer</button>
+                    </a></td>
+            </tr>
+            <?php
+        }
+        ?>
+        </tbody>
+    </table>
 
-
-<?php
-
-  if (isset($_GET['del'])) {
-      deleteComputer($_GET['del']);
-  }
+    <?php
+    if (isset($_GET['del'])) {
+        deleteComputer($_GET['del']);
     }
+}
