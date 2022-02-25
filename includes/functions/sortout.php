301e480dcf0930da089d0301675c13c7cc76483b
jbenga <jpaul.benga@gmail.com>
2022-02-25 10:15:57 +0100

    mini correction

--- includes/functions/sortout.php
+++ includes/functions/sortout.php
@@ -3,71 +3,16 @@
  * TRIS
  ***********/
 
-function priceAsc(string $col, string $table){
+ function orderBy(string $col, string $table, string $order = "ASC"): array {
     $con = getDataBaseConnexion();
-    $request = "SELECT $col FROM $table ORDER BY price ASC";
+    $request = "SELECT * FROM $table ORDER BY $col $order";
     $stmt = $con->query($request);
-    return $stmt->fetch();
-}
-
- function priceDesc(string $col, string $table){
-    $con = getDataBaseConnexion();
-    $request = "SELECT $col FROM $table ORDER BY price DESC";
-    $stmt = $con->query($request);
-    return $stmt->fetch();
-}
-
-function nameAsc(string $table){
-    $con = getDataBaseConnexion();
-    $request = "SELECT name FROM $table ORDER BY name ASC"; 
-    $stmt = $con->query($request);
-    return $stmt->fetch();
-}
-
-function nameDesc(string $table){
-    $con = getDataBaseConnexion();
-    $request = "SELECT name FROM $table ORDER BY name DESC"; 
-    $stmt = $con->query($request);
-    return $stmt->fetch();
-}
-
-function recentAsc(string $table){
-    $con = getDataBaseConnexion();
-    $request = "SELECT dateAdd FROM $table ORDER BY dateAdd ASC"; 
-    $stmt = $con->query($request);
-    return $stmt->fetch();
-}
-
-function recentDesc(string $table){
-    $con = getDataBaseConnexion();
-    $request = "SELECT dateAdd FROM $table ORDER BY dateAdd DESC"; 
-    $stmt = $con->query($request);
-    return $stmt->fetch();
+    return $stmt->fetchAll();
 }
 
 function stockAsc(){}
 
 function stockDesc(){}
-
-function quantityAsc(string $table){
-    $con = getDataBaseConnexion();
-    $request = "SELECT quantity FROM $table ORDER BY quantity ASC"; 
-    $stmt = $con->query($request);
-    return $stmt->fetch();
-}
-
-function quantityDesc(string $table){
-    $con = getDataBaseConnexion();
-    $request = "SELECT quantity FROM $table ORDER BY quantity ASC"; 
-    $stmt = $con->query($request);
-    return $stmt->fetch();
-}
-
-
-
-// fonction nombre de modèle créer avec cette pièce
-
-
 /************
  * FILTRES
  ***********/
